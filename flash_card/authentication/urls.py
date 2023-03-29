ee12b4f32854d41ebe003bddaaaebfc6b6333572
bahmanpnn <bahmanpn@gmail.com>
2023-03-30 02:09:45 +0330

    add register url and endpoint completed

--- flash_card/authentication/urls.py
+++ flash_card/authentication/urls.py
@@ -1,7 +1,9 @@
 from rest_framework_simplejwt import views as jwt_views
 from django.urls import path
+from .views import *
 
 urlpatterns = [
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path("register/",CreateUser.as_view(),name='create-user'),
 ]
--- flash_card/authentication/views.py
+++ flash_card/authentication/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import CreateUserProfileSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+#because of error in saving password with createapiview we should change the codes and set APIView
+# if go to admin panel see this error in password field of new user==>
+# Invalid password format or unknown hashing algorithm. 
+
+# class CreateUser(CreateAPIView):
+#     queryset=User.objects.all()
+#     serializer_class=CreateUserProfileSerializer
+
+class CreateUser(APIView):
+    def post(self,request):
+        req_data=request.data
+
+        serializer=CreateUserProfileSerializer(data=req_data)
+        serializer.is_valid(raise_exception=True)
+        data=serializer.validated_data
+
+        user_data=User(
+            username=data['username'],
+            email=data['email'],
+        )
+        user_data.set_password(data['password'])
+        user_data.save()
+
+        return Response(serializer.data)

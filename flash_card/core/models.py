441dbf8184521f0f92437b964a788ab35bd67043
bahmanpnn <bahmanpn@gmail.com>
2023-03-30 00:54:27 +0330

    add flashcard model and data and config restframework

--- flash_card/core/models.py
+++ flash_card/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class FlashCard(models.Model):
+    question=models.TextField()
+    answer=models.TextField()
+    created_at=models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name = "flashcard"
+        verbose_name_plural = "flashcards"
+
+    def __str__(self):
+        if len(self.question)>25:
+            return f'{self.question[0:25]}...'
+        else:
+            return f'{self.question}'
+
